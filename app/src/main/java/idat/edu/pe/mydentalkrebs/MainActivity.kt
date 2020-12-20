f0584506dd7781259cae41b472e07d981a867e14
Rodriguez1995 <Grodriguez1905@hotmail.com>
2020-12-20 12:50:51 -0500

    Add snackbar and override fun onBackPressed

--- app/src/main/java/idat/edu/pe/mydentalkrebs/MainActivity.kt
+++ app/src/main/java/idat/edu/pe/mydentalkrebs/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import idat.edu.pe.mydentalkrebs.PreferenceHelper.get
 import idat.edu.pe.mydentalkrebs.PreferenceHelper.set
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val snackBar by lazy {
+        Snackbar.make(mainLayout, R.string.press_back_again, Snackbar.LENGTH_SHORT)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +30,6 @@ class MainActivity : AppCompatActivity() {
         val session = preferences.getBoolean("session",false)
         */
         val preferences = PreferenceHelper.defaultPrefs(this)
-
         if (preferences["session", false]!!)
             goToMenuActivity()
 
@@ -58,5 +62,13 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this,MenuActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    override fun onBackPressed() {
+        if (snackBar.isShown)
+            super.onBackPressed()
+        else
+            snackBar.show()
+
     }
 }
